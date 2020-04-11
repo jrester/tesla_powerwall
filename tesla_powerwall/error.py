e59fc8e2b9e7a1203b6409bfe34260ac80d66f23
Jrester <jrester379@gmail.com>
2020-04-11 16:50:46 +0200

    Use base class for powerwall errors

--- tesla_powerwall/error.py
+++ tesla_powerwall/error.py
@@ -1,16 +1,24 @@
-class ApiError(Exception):
+class PowerwallError(Exception):
+    def __init__(self, msg):
+        super().__init__(msg)
+
+class ApiError(PowerwallError):
     def __init__(self, error):
         super().__init__(f"Powerwall api error: {error}")
 
 
-class PowerwallUnreachableError(Exception):
-    def __init__(self):
-        super().__init__(f"Powerwall is unreachable!")
+class PowerwallUnreachableError(PowerwallError):
+    def __init__(self, reason=None):
+        msg = f"Powerwall is unreachable"
+        if reason is not None:
+            self.reason = reason
+            msg = f"{msg}: {reason}"
+        super().__init__(msg)
 
 
-class AccessDeniedError(Exception):
+class AccessDeniedError(PowerwallError):
     def __init__(self, resource, error=None):
         msg = f"Access denied for resource {resource}"
-        if msg is not None:
+        if error is not None:
             msg = f"{msg}: {error}"
         super().__init__(msg)
